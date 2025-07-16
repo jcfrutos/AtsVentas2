d913dd5841e72bd401771b48259fefac71a8763d
fdquinones1986 <140764717+fdquinones1986@users.noreply.github.com>
2025-07-16 02:27:34 +0000

    ajustes para validar archivo excel

--- atsventas/src/main/java/anexostransaccionales/atsventas/controller/AtsVentasController.java
+++ atsventas/src/main/java/anexostransaccionales/atsventas/controller/AtsVentasController.java
@@ -13,14 +13,19 @@ import anexostransaccionales.atsventas.models.entities.AtsVentas;
 import anexostransaccionales.atsventas.service.AtsVentasService;
 import anexostransaccionales.atsventas.service.EmailService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.http.MediaType; // Import MediaType
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content; // Import Content
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/atsventas")
@@ -32,10 +37,11 @@ public class AtsVentasController {
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/import")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, path = "/import")
     @Operation(summary = "Permite importar un archivo XML de ATS Ventas",
                description = "Este endpoint permite importar un archivo XML que contiene datos de ATS Ventas. "
-                           + "El archivo debe contener un solo nodo raíz para ser procesado correctamente.")
+                           + "El archivo debe contener un solo nodo raíz para ser procesado correctamente.",
+               requestBody = @RequestBody(content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)))
     public ResponseEntity<?> importAtsVentas(@RequestParam("file") MultipartFile file) {
         String result = atsVentasService.importAtsVentasFromFile(file);
         if (result.startsWith("el archivo xml debe contener un solo nodo")) {
